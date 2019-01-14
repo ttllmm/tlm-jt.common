67847a085606f91b5919792ccc29043d08284cc4
tlm <568114272@qq.com>
2019-01-14 18:59:04 +0800

    2019.01.14.18:58

---
+++ src/main/java/_jt_spring_boot/commen/App.java
@@ -0,0 +1,13 @@
+package _jt_spring_boot.commen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
